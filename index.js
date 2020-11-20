a8a747c6cd34881c3b3842482c5630bbcbf20f9c
gitismatt <mmaynard@alum.lehigh.edu>
2020-11-20 03:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase()===string) {
-    return("I can't hear you!")
-  }if (string.toUpperCase()===string) {
-    return("YES INDEED!")
-  }if string==="I love you, Grandma." {
-    return("I love you, too.")
-  }
+  function sayHiToGrandma(string) {
+    if (string.toLowerCase() === string) {
+      return "I can't hear you!"
+    }
 
+    if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+    }
+
+    if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+
+    return "Are you eating enough?"
+  }
 }
